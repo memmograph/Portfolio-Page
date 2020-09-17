fa5022e729c8bd8146663de32ac9518ba68c7182
Mike Emmons <memmons18@gmail.com>
2020-09-17 14:35:37 -0400

    Updates

---
+++ precache-manifest.3bb89e0a515b6109c139acd8d00908ca.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc84fe05d26ee676f8f0821c5777260",
+    "url": "/portfolio-page/index.html"
+  },
+  {
+    "revision": "28346e280add68aa49db",
+    "url": "/portfolio-page/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c1b5588e60b91ac9cd1a",
+    "url": "/portfolio-page/static/css/main.4c13dba9.chunk.css"
+  },
+  {
+    "revision": "28346e280add68aa49db",
+    "url": "/portfolio-page/static/js/2.9c6f2592.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-page/static/js/2.9c6f2592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1b5588e60b91ac9cd1a",
+    "url": "/portfolio-page/static/js/main.9f31956d.chunk.js"
+  },
+  {
+    "revision": "d81a56928d08a255fc4a",
+    "url": "/portfolio-page/static/js/runtime-main.a1ed60d5.js"
+  },
+  {
+    "revision": "b390bde548ad32e66065a9904315b1a1",
+    "url": "/portfolio-page/static/media/EmployeeTracker.b390bde5.gif"
+  },
+  {
+    "revision": "4b8a998e2c1be1c8171a25c5bf2abd90",
+    "url": "/portfolio-page/static/media/LauraLaraAmaya_Resume.4b8a998e.pdf"
+  },
+  {
+    "revision": "51d2dc2db99138838d83b2b4db9be274",
+    "url": "/portfolio-page/static/media/Workout-tracker.51d2dc2d.gif"
+  },
+  {
+    "revision": "1f719141e2dac64e7ab434501efcb16c",
+    "url": "/portfolio-page/static/media/background1.1f719141.jpg"
+  },
+  {
+    "revision": "c528a1541a117c70eedd1feb1961ef2d",
+    "url": "/portfolio-page/static/media/birthday.c528a154.gif"
+  },
+  {
+    "revision": "81304af16d5037c11d01d84ef446bb36",
+    "url": "/portfolio-page/static/media/burger.81304af1.gif"
+  },
+  {
+    "revision": "56a3d8d37829f46b966baf38ea531194",
+    "url": "/portfolio-page/static/media/employee-directory.56a3d8d3.gif"
+  },
+  {
+    "revision": "182881ae41b89875d0d2f6c9ffd501af",
+    "url": "/portfolio-page/static/media/fitness-tracker.182881ae.gif"
+  },
+  {
+    "revision": "ad45e2b15dc3c708287bc63bae0f9b61",
+    "url": "/portfolio-page/static/media/laura1.ad45e2b1.jpg"
+  },
+  {
+    "revision": "079882ff046c60fd7f376761efe9ec75",
+    "url": "/portfolio-page/static/media/notes.079882ff.gif"
+  },
+  {
+    "revision": "1a3a635ebceee80b0179a233cac99d7c",
+    "url": "/portfolio-page/static/media/planner.1a3a635e.gif"
+  },
+  {
+    "revision": "d9b815b08dd26708b5133ce5b69105bd",
+    "url": "/portfolio-page/static/media/weather.d9b815b0.gif"
+  }
+]);
