89abb5c7b36f140d53e00bfe8cacef7a69a7e135
Laura Lara <llaraama@uncc.edu>
2020-09-17 03:24:55 -0400

    deployment test 3

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './style.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'https://fonts.googleapis.com/css2?family=Libre+Caslon+Display&display=swap'
-// import 'https://use.fontawesome.com/7267342c00.js'
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
