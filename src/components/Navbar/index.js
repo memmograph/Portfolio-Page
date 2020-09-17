870bba7256c063dc024c059624018470b2d8caba
Mike Emmons <memmons18@gmail.com>
2020-09-17 14:07:22 -0400

    added scripts

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,23 +1,23 @@
 import React from "react"
 
-function Navbar() {
-  return (
-    <nav className="menu-wrap">
-      <input type="checkbox" className="toggler"></input>
-      <div className="hamburger"><div></div></div>
-      <div className="menu">
-        <div>
+function Navbar (){
+    return(
+      <nav className="menu-wrap">
+        <input type="checkbox" className="toggler"></input>
+        <div className="hamburger"><div></div></div>
+        <div className="menu">
           <div>
-            <ul>
-              <li><a href="/Portfolio-Page/">Home</a></li>
-              <li><a href="/Portfolio-Page//about">About</a></li>
-              <li><a href="/portfolio">Portfolio</a></li>
-              <li><a href="/contact">Contact</a></li>
-            </ul>
+            <div>
+              <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/portfolio">Portfolio</a></li>
+                <li><a href="/contact">Contact</a></li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
-  )
+      </nav>
+    )
 }
 export default Navbar
